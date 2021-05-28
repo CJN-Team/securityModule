07e342ec460111fd901583ee94a8fde405a55687
James Morales <jamesmoralesmoreno@gmail.com>
2021-05-28 18:28:14 -0500

    pequeños cambios para visualizar el video

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template, Response,  make_response, request,redirect,url_for
+from flask import Flask, jsonify, render_template, Response,  make_response, request, redirect, url_for
 import copy
 import numpy as np
 import cv2
@@ -6,15 +6,15 @@ import base64
 import glob
 
 
-
 from Prediction import Prediction
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='static')
 predictionImage = Prediction()
 
 released = False
 
-frames=[]
+frames = []
+
 
 @app.route('/')
 def index():
@@ -22,9 +22,21 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/report')
+def lastReport():
+
+    return render_template('index.html', title="page", jsonfile=json.dumps(data))
+
+
+@app.route('/video')
+def lastVideo():
+
+    return render_template('video.html')
+
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    global released,frames
+    global released, frames
 
     if request.method == 'POST':
         fs = request.files.get('snap')
@@ -36,12 +48,12 @@ def upload():
             imgEmotions = predictionImage.emotionDetection(copy.deepcopy(img))
             imgObjects = predictionImage.objectDetection(copy.deepcopy(img))
             imgFaces = predictionImage.facialDetection(copy.deepcopy(img))
-            imgBehavior = predictionImage.behaviorDetection(copy.deepcopy(img))
-
-            cv2.resize(imgEmotions, (480, 480),interpolation=cv2.INTER_CUBIC)
-            cv2.resize(imgObjects, (480, 480),interpolation=cv2.INTER_CUBIC)
-            cv2.resize(imgFaces, (480, 480),interpolation=cv2.INTER_CUBIC)
-            cv2.resize(imgBehavior, (480, 480),interpolation=cv2.INTER_CUBIC)
+            #imgBehavior = predictionImage.behaviorDetection(copy.deepcopy(img))
+            imgBehavior = copy.deepcopy(img)
+            cv2.resize(imgEmotions, (480, 480), interpolation=cv2.INTER_CUBIC)
+            cv2.resize(imgObjects, (480, 480), interpolation=cv2.INTER_CUBIC)
+            cv2.resize(imgFaces, (480, 480), interpolation=cv2.INTER_CUBIC)
+            cv2.resize(imgBehavior, (480, 480), interpolation=cv2.INTER_CUBIC)
 
             imgEmotions = np.array(imgEmotions)
             imgEmotions = imgEmotions.astype('uint8')
@@ -53,13 +65,11 @@ def upload():
             imgBehavior = imgBehavior.astype('uint8')
 
             vid1 = cv2.hconcat(
-                [imgEmotions,imgObjects ])
+                [imgEmotions, imgObjects])
             vid2 = cv2.hconcat(
-                [imgFaces,imgBehavior ])
+                [imgFaces, imgBehavior])
 
-            
             frame = cv2.vconcat([vid1, vid2])
-            
 
             cv2.resize(frame, (1280, 720))
 
@@ -70,44 +80,40 @@ def upload():
             _, imgFacesEncoded = cv2.imencode('.jpg', imgFaces)
             _, imgBehaviorEncoded = cv2.imencode('.jpg', imgBehavior)
 
-            
-            predictionImage.frames+=1
+            predictionImage.frames += 1
 
             buf = jsonify({'img': base64.b64encode(imgEmotionsEncoded).decode('ascii'), 'img2': base64.b64encode(imgObjectsEncoded).decode(
                 'ascii'), 'img3': base64.b64encode(imgFacesEncoded).decode('ascii'), 'img4': base64.b64encode(imgBehaviorEncoded).decode('ascii')})
 
-            released= False
+            released = False
             return buf
         else:
             if (released == False):
                 makeVideo()
-            
+
             return 'App stopped'
 
     return 'Hello World!'
 
+
 def makeVideo():
     global frames
-    
+
     height, width, _ = frames[0].shape
-    size = (width,height)
-    
-    
-    out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 3, size)
-    
+    size = (width, height)
+
+    out = cv2.VideoWriter(
+        'static/project.avi', cv2.VideoWriter_fourcc(*'DIVX'), 3, size)
+
     for i in range(len(frames)):
         out.write(frames[i])
     out.release()
 
-    
     predictionImage.makeReport()
-    frames=[]
-    predictionImage.frames=0
-
-
+    frames = []
+    predictionImage.frames = 0
 
     cv2.destroyAllWindows()
-    
 
 
 if __name__ == "__main__":
